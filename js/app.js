9a133eb29bdac1ba7ece05539a03b956f05ba918
michael-ryabokon <michael.ryabokon@gmail.com>
2014-08-07 23:10:40 +0300

    resize + ng-grid

--- js/app.js
+++ js/app.js
@@ -1,15 +1,50 @@
-var payment = angular.module('payment', []);
+var payment = angular.module('payment', ['ngGrid']);
 
-payment.controller('MainContrller', function ($scope) {
-	$scope.resize = function () {
-		debugger;
+payment.controller('PaymentMain', function ($scope) {
+	$scope.rows = [{
+		season: 'summer'
+		// ,months: ['June', 'July', 'August']
+	}];
+
+	$scope.gridOptions = {
+		data: 'rows'
 	}
+
 });
 
-payment.directive('resize', ['$window', function ($window) {
-	var _window = angular.element($window);
 
-	return function (scope, element, attrs) {
-		_window.on('resize', scope.resize); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+payment.directive('resize', ['$window', function ($window) {
+	var $window = angular.element($window);
+
+	function resize (event) {
+		var width = $(event.currentTarget).width(),
+			period = 'season'; // half-year, year
+
+			if (width >= 767 && width <= 991) { 
+				console.log('period - half-year');
+			} else if (width >= 922) {
+				console.log('period - full yer');
+			} else {
+				console.log('season');
+			}
+	};
+
+	return {
+		link: function () {
+			$window.on('resize', resize);
+		}
 	}
 }]);
